da48c23753e61a2bd79348fc5182e575dbfb0fa6
Tushar <tusharofficial96@gmail.com>
2023-12-05 03:08:40 -0800

    updated code with ontology

--- dashboard.py
+++ dashboard.py
@@ -4,67 +4,66 @@ from rdflib.plugins.sparql import prepareQuery
 from rdflib import Graph, Literal, Namespace, RDF, URIRef
 
 # Create a namespace for the project
-ex = Namespace("http://example.org/")
+ns1 = Namespace("http://example.org/")
 
 def recommend_laptops():
+
     # Retrieve user preferences from the entry fields
-    laptop_type_preference = laptop_type_preference.get()
-    processor_preference = processor_entry.get()
-    ram_preference = ram_entry.get()
-    storage_preference = storage_entry.get()
-    category_preference = category_entry.get()
+    user_laptop_type_preference = laptop_type_preference.get()
+    # processor_preference = processor_entry.get()
+    # ram_preference = ram_entry.get()
+    # storage_preference = storage_entry.get()
+    # category_preference = category_entry.get()
     budget_preference = budget_entry.get()
 
     # Create a new RDF graph and add user preferences
     user_graph = Graph()
     user_uri = URIRef(f"http://example.org/User1")  # You can use a dynamic user ID
-    user_graph.add((user_uri, RDF.type, ex.User))
-    user_graph.add((user_uri, ex.prefersProcessor, Literal(processor_preference)))
-    user_graph.add((user_uri, ex.prefersRAM, Literal(ram_preference)))
-    user_graph.add((user_uri, ex.prefersStorage, Literal(storage_preference)))
-    user_graph.add((user_uri, ex.prefersCategory, Literal(category_preference)))
-    user_graph.add((user_uri, ex.hasBudget, Literal(budget_preference)))
+    user_graph.add((user_uri, RDF.type, ns1.User))
+    # user_graph.add((user_uri, ex.prefersProcessor, Literal(processor_preference)))
+    # user_graph.add((user_uri, ex.prefersRAM, Literal(ram_preference)))
+    # user_graph.add((user_uri, ex.prefersStorage, Literal(storage_preference)))
+    # user_graph.add((user_uri, ex.prefersCategory, Literal(category_preference)))
+    user_graph.add((user_uri, ns1.prefersLaptopType, Literal(user_laptop_type_preference)))
+    user_graph.add((user_uri, ns1.hasBudget, Literal(budget_preference)))
 
     # Merge user preferences into the main graph
     g = Graph()
     g.parse("ontology.ttl", format="turtle")  # Load ontology
     g.parse("data.ttl", format="turtle")  # Load existing data
-    g.parse("users.ttl", format="turtle")  # Load existing user data
+    # g.parse("users.ttl", format="turtle")  # Load existing user data
     g += user_graph
 
     # SPARQL query to get laptop recommendations based on user preferences
-    query_str = f"""
-        SELECT ?laptop
-        WHERE {{
-            {user_uri} a ex:User ;
-                ex:prefersProcessor ?processor ;
-                ex:prefersRAM ?ram ;
-                ex:prefersStorage ?storage ;
-                ex:prefersCategory ?category ;
-                ex:hasBudget ?budget .
+    query_str = """
+                SELECT ?ram ?brand ?model
+                WHERE {
+                ?laptop a ns1:Laptop ;
+                        ns1:ram ?ram ;
+                        ns1:brand ?brand ;
+                        ns1:model ?model ;
+                        
+                        ns1:hd_size ?storage ;
+                        ns1:processor_brand ?processor .
 
-            ?laptop a ex:Laptop ;
-                ex:hasProcessor ?processor ;
-                ex:hasRAM ?ram ;
-                ex:hasStorage ?storage ;
-                ex:belongsToCategory ?category ;
-                ex:hasPrice ?price .
-            
-            FILTER (?price <= ?budget)
-        }}
-    """
+                FILTER (?ram > 8 && ?storage > 500 && (regex(?processor, "intel", "i") || regex(?processor, "intel", "i")))
+                }
+                """
 
-    query = prepareQuery(query_str, initNs={"ex": ex})
+    query = prepareQuery(query_str, initNs={"ns1": ns1})
     results = g.query(query)
 
     # Print or use the recommendations as needed (for demonstration purposes)
+    print(len(results))
+
     for row in results:
-        print(row[0])
+        print('....',row.ram, row.brand, row.model)
     
     # Display the recommendations in the text box
     recommendations_text.delete(1.0, tk.END)  # Clear previous content
+    recommendations_text.insert(tk.END,"Here are the results....")
     for row in results:
-        recommendations_text.insert(tk.END, f"{row[0]}\n")
+        recommendations_text.insert(tk.END, f"{row.brand} {row.model}\n")
 
 # Create the main window
 root = tk.Tk()
