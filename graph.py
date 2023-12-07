8319b5f9b0a5bb0a66e43ec1fbee43c77e402158
iamdhrutipatel <dhrutipatel0100@gmail.com>
2023-12-07 14:12:11 -0800

    made some file changes in graph.py & process_csv_data.py

--- graph.py
+++ graph.py
@@ -12,24 +12,39 @@ from rdflib.extras.external_graph_libs import rdflib_to_networkx_multidigraph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# Create an RDF graph and load ontology
-g = Graph()
-g.parse("ontology.ttl", format="turtle")
+def visualize_rdf_graph(ontology_file, data_file):
+    """
+    Visualize RDF Graph using Networkx and Matplotlib.
 
-# Load existing data into the RDF graph
-result = g.parse("data_graph.ttl", format="turtle")
+    Parameters:
+    - ontology_file (str): Path to the ontology file in Turtle format.
+    - data_file (str): Path to the RDF data file in Turtle format.
 
-# Define a user URI for demonstration
-user_uri = URIRef(f"http://example.org/User1")
+    """
+    # Create an RDF graph and load ontology
+    g = Graph()
+    g.parse(ontology_file, format="turtle")
 
-# Convert RDF graph to Networkx multidigraph
-G = rdflib_to_networkx_multidigraph(result)
+    # Load existing data into the RDF graph
+    result = g.parse(data_file, format="turtle")
 
-# Plot Networkx instance of RDF Graph
-pos = nx.spring_layout(G, scale=2)
-edge_labels = nx.get_edge_attributes(G, 'r')
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
-nx.draw(G, with_labels=True)
+    # Define a user URI for demonstration
+    user_uri = URIRef("http://example.org/User1")
 
-# Display the plot
-plt.show()
+    # Convert RDF graph to Networkx multidigraph
+    G = rdflib_to_networkx_multidigraph(result)
+
+    # Plot Networkx instance of RDF Graph
+    pos = nx.spring_layout(G, scale=2)
+    edge_labels = nx.get_edge_attributes(G, 'r')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    nx.draw(G, with_labels=True)
+
+    # Display the plot
+    plt.show()
+
+# Example usage
+if __name__ == "__main__":
+    ontology_path = "ontology.ttl"
+    data_path = "data_graph.ttl"
+    visualize_rdf_graph(ontology_path, data_path)
--- process_csv_data.py
+++ process_csv_data.py
@@ -9,39 +9,52 @@ and serializes the RDF graph to Turtle format.
 import pandas as pd
 from rdflib import Graph, URIRef, Literal, Namespace, RDF, XSD
 
-# Create a namespace for the project
-ex = Namespace("http://example.org/")
+def csv_to_rdf(csv_file, output_file):
+    """
+    Convert laptop data from CSV to RDF and save it to a file.
 
-# Read the CSV data
-csv_file = "laptops.csv"
-df = pd.read_csv(csv_file)
+    Parameters:
+    - csv_file (str): Path to the CSV file containing laptop data.
+    - output_file (str): Path to the output RDF file in Turtle format.
 
-# Create an RDF graph
-g = Graph()
+    """
+    # Create a namespace for the project
+    ex = Namespace("http://example.org/")
 
-# Define the RDF type for laptops
-laptop_type = ex.Laptop
+    # Read the CSV data
+    df = pd.read_csv(csv_file)
 
-# Iterate through the rows in the CSV and create RDF triples
-for index, row in df.iterrows():
-    laptop_uri = ex[f"Laptop_{row['id']}"]
-    g.add((laptop_uri, RDF.type, laptop_type))
-    
-    # Add other properties as triples
-    for column, value in row.items():
-        if pd.notna(value):  # Skip NaN values
-            property_uri = ex[column]
+    # Create an RDF graph
+    g = Graph()
 
-            # Handle numeric values with appropriate datatypes
-            if column in ['weight', 'screen_size', 'graphic_card_size', 'clock_speed']:
-                value_literal = Literal(value, datatype=XSD.float)
-            else:
-                value_literal = Literal(value)
+    # Define the RDF type for laptops
+    laptop_type = ex.Laptop
 
-            g.add((laptop_uri, property_uri, value_literal))
+    # Iterate through the rows in the CSV and create RDF triples
+    for index, row in df.iterrows():
+        laptop_uri = ex[f"Laptop_{row['id']}"]
+        g.add((laptop_uri, RDF.type, laptop_type))
+        
+        # Add other properties as triples
+        for column, value in row.items():
+            if pd.notna(value):  # Skip NaN values
+                property_uri = ex[column]
 
-# Serialize the RDF graph to Turtle format and save it to a file
-output_file = "data.ttl"
-g.serialize(output_file, format="turtle")
+                # Handle numeric values with appropriate datatypes
+                if column in ['weight', 'screen_size', 'graphic_card_size', 'clock_speed']:
+                    value_literal = Literal(value, datatype=XSD.float)
+                else:
+                    value_literal = Literal(value)
 
-print(f"RDF data has been saved to {output_file}")
+                g.add((laptop_uri, property_uri, value_literal))
+
+    # Serialize the RDF graph to Turtle format and save it to a file
+    g.serialize(output_file, format="turtle")
+
+    print(f"RDF data has been saved to {output_file}")
+
+# Example usage
+if __name__ == "__main__":
+    csv_path = "laptops.csv"
+    rdf_output_path = "data.ttl"
+    csv_to_rdf(csv_path, rdf_output_path)
